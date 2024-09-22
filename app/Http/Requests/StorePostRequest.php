df498d699eb77164e260ae708d47bf794646d675
Aldair Neves <aldairneves608@gmail.com>
2024-09-21 22:33:53 -0300

    Project Tested

---
+++ app/Http/Requests/StorePostRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePostRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'title_anotacao' => ['required', 'max:70'],
+            'conteudo' => ['required'],
+            'status' => ['required']
+        ];
+    }
+}
---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title_anotacao' => $this->title_anotacao,
+            'conteudo' => $this->content,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
