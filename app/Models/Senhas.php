72908a0124e8694a430de6add31c83fa2c0b17c8
Aldair Neves <aldairneves608@gmail.com>
2024-10-19 20:34:27 -0300

    ajustes

--- app/Models/Senhas.php
+++ app/Models/Senhas.php
@@ -9,7 +9,17 @@ class Senhas extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['plataforma', 'email', 'senha', 'email_recovery', 'date_generation', 'senha_valido', 'senha_alterada', 'motivo', 'data_alteracao', 'chaves_id'];
+    protected $fillable = [
+        'plataforma',
+        'email',
+        'senha',
+        'email_recovery',
+        'date_generation',
+        'senha_valido',
+        'senha_alterada',
+        'motivo',
+        'data_alteracao',
+        'chaves_id'];
 
     public function chaves()
     {
